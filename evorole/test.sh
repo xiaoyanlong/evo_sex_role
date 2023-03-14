0c9a1f4220409d0655d88a6b49a976b49916e420
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-03-14 19:51:17 +0100

    job submmission according to bwhpc

--- evorole/test.sh
+++ evorole/test.sh
@@ -2,12 +2,12 @@
 
 #!!! adjust the time quota !!!
 
-#SBATCH --time=00:15:00
+#SBATCH --time=15:00
 #SBATCH --ntasks=1
 #SBATCH --mem=1000
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=dev_multiple
 
-datadir="~/"
+datadir="/home/fr/fr_fr/fr_xl1011/evo_sex_role/evorole"
 ./bin/evorole --force param=param.json rep=10 outdir=$datadir/data
 
