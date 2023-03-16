4c9d9781eb26ff8bc4c859e63aeb4d0556c9dddc
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-03-16 17:01:40 +0100

    job submmission according to bwhpc

--- evorole/test.sh
+++ evorole/test.sh
@@ -9,6 +9,6 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=single
 
-datadir="/$(whoami) /pfs/work7"
-./bin/evorole --force param=param.json rep=10 outdir=$datadir/evosexrole/data/2023-03-16/premating_initial5_m0_prefemale1
+datadir="$(whoami)/pfs/work7"
+./bin/evorole --force param=param.json rep=10 outdir=$datadir/2023-03-16_premating_initial5_m0_prefemale1
 
