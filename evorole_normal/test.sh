eed08793871b7dbad234d7753070361beef825b9
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-05-15 17:59:51 +0200

    normal dis

---
+++ evorole_normal/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!!! adjust the time quota !!!
+
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=5000
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=single
+
+datadir="/pfs/work7/workspace/scratch/fr_xl1011-data/evosexrole"
+./bin/evorole --force param=param.json rep=10 outdir=$datadir/2023-03-16_premating_initial5_m0_prefemale1
+
