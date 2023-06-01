946f30e1c9a1b71349eae6ad4a25c3d7769928a4
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-06-01 19:47:26 +0200

    normal dis

---
+++ evorole_normal/2023-06-01_sexual_selection_sd_pre_orna_0.5_sd_care_0.5_4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!!! adjust the time quota !!!
+
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=8000
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=single
+
+datadir="/pfs/work7/workspace/scratch/fr_xl1011-data/evosexrole_normal"
+./bin/evorole --force param=2023-06-01_sexual_selection_sd_pre_orna_0.5_sd_care_0.5.json rep=2 outdir=$datadir/2023-06-01_sexual_selection_sd_pre_orna_0.5_sd_care_0.5_4
+
