a4de81b827783ab0e3542781118e3303d888af81
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-06-13 16:32:09 +0200

    Fig.7 check

--- evorole/2023-06-12_premating_original_mu0.0_prefemale0.sh
+++ evorole/2023-06-12_premating_original_mu0.0_prefemale0.sh
@@ -9,7 +9,6 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=single
 #SBATCH --array=1-5:1
-#SBATCH -o slurm.%N.%J.%u.%a.out # STDOUT
 
 datadir="/pfs/work7/workspace/scratch/fr_xl1011-data/evosexrole"
 ./bin/evorole --force param=2023-06-12_premating_original_mu0.0_prefemale0.json rep=20 outdir=$datadir/2023-06-12_premating_original_mu0.0_prefemale0_${SLURM_ARRAY_TASK_ID}
