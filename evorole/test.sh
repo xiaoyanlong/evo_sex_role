b0ac8dc94ee7709512e44f40f923b036dd031cdd
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-06-13 19:21:36 +0200

    Fig.7 check

--- evorole/test.sh
+++ evorole/test.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --mem=5000
 #SBATCH --cpus-per-task=16
-#SBATCH --partition=single
+#SBATCH --partition=gpu_8
 
 datadir="/pfs/work7/workspace/scratch/fr_xl1011-data/evosexrole"
 ./bin/evorole --force param=param.json rep=10 outdir=$datadir/2023-03-16_premating_initial5_m0_prefemale1
