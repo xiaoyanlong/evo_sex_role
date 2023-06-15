94e263b080f1675061a86184cb1aa658f1ec6f78
richelbilderbeek <xiaoyan.long2@gmail.com>
2023-06-15 15:26:11 +0200

    Fig.7 check

--- evorole/2023-06-12_premating_original_mu0.001_prefemale3.sh
+++ evorole/2023-06-12_premating_original_mu0.001_prefemale3.sh
@@ -2,14 +2,12 @@
 
 #!!! adjust the time quota !!!
 
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --mem=8000
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=single
-#SBATCH --array=1-10:1
 
 datadir="/pfs/work7/workspace/scratch/fr_xl1011-data/evosexrole"
-./bin/evorole --force param=2023-06-12_premating_original_mu0.001_prefemale3.json rep=10 outdir=$datadir/2023-06-12_premating_original_mu0.001_prefemale3_${SLURM_ARRAY_TASK_ID}
-
+./bin/evorole --force param=2023-06-12_premating_original_mu0.001_prefemale3.json rep=100 outdir=$datadir/2023-06-12_premating_original_mu0.001_prefemale3
--- evorole/submit_example - Copy.sh
+++ evorole/submit_example - Copy.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-
-#!!! adjust the time quota !!!
-
-#SBATCH --time=00:15:00
-#SBATCH --ntasks=1
-#SBATCH --nodes=1
-#SBATCH --mem=1000
-#SBATCH --cpus-per-task=16
-
-module load GCC --latest
-
-datadir="/data/$(whoami)"
-./bin/evorole --force param=param.json rep=10 outdir=$datadir/evorole
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
